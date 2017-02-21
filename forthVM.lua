e5e873f40dfc41a1ac48e0a5e193ad696eef65f4
HexBlues <hifreeblues@hotmail.com>
2017-02-21 21:39:38 +0800

    base version

--- forthVM.lua
+++ forthVM.lua
@@ -1,19 +1,7 @@
+-- Forth VM
+
 -- 求值函数
 eval = function (str) return assert(load(str))() end
-
--- 栈和入栈,出栈函数的定义
-DS = { n = 0 }
-push = function (stack, x) stack.n = stack.n + 1; stack[stack.n] = x end
-pop  = function (stack) local x = stack[stack.n]; stack[stack.n] = nil;
-                          stack.n = stack.n - 1; return x end
-
--- 字典的定义, 这里暂时直接把 5 DUP * . 每个字都当做原语来定义
-_F = {}
-_F[""]   = function () mode = "stop" end
-_F["5"]   = function () push(DS, 5) end
-_F["DUP"] = function () push(DS, DS[DS.n]) end
-_F["*"]   = function () push(DS, pop(DS) * pop(DS)) end
-_F["."]   = function () print(" "..pop(DS)) end
 
 -- 语法解析, 把代码文本解析为一个个的字 word 和空格
 parser = function (pat)
@@ -29,6 +17,22 @@ parseWordOrNewline = function () return parseWord() or parseNewline() end
 -- 在 Forth 中, 一个字是一个非空字符序列, 外部解释器每次读取一个字然后执行它
 getWord          = function () parseSpaces(); return parseWord() end
 getWordOrNewline = function () parseSpaces(); return parseWordOrNewline() end
+
+
+-- 栈和入栈,出栈函数的定义
+DS = { n = 0 }
+push = function (stack, x) stack.n = stack.n + 1; stack[stack.n] = x end
+pop  = function (stack) local x = stack[stack.n]; stack[stack.n] = nil;
+                          stack.n = stack.n - 1; return x end
+
+-- 字典的定义, 这里暂时直接把 5 DUP * . 每个字都当做原语来定义
+_F = {}
+_F["\n"] = function () end
+_F[""]   = function () mode = "stop" end
+_F["5"]   = function () push(DS, 5) end
+_F["DUP"] = function () push(DS, DS[DS.n]) end
+_F["*"]   = function () push(DS, pop(DS) * pop(DS)) end
+_F["."]   = function () print(" "..pop(DS)) end
 
 
 -- 定义模式表
@@ -59,6 +63,7 @@ run = function () while mode ~= "stop" do modes[mode]() end end
 
 -- 用户输入的代码文本, 也就是准备在虚拟机中执行的程序
 subj = [==[
+	5 DUP * .
 	5 DUP * .
 ]==]
     
